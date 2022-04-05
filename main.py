36cc949bdc6deed4f973507474654e647ad7a4ea
Oscar_WorkLaptop <oscar.pacheco@ultrabox.com>
2022-04-05 10:38:43 -0500

    API example commited

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from flask import Flask, request
+from flask_restful import abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+vector = [1, 2, 3, 4, 5]
+class RecObj(Resource):
+    def get(self):
+        return vector
+
+    def post(self):
+        print (request.json)
+        vector.append(int(request.json["num"]))
+        return f"El número adjuntado fue: {request.json['num']}"
+
+
+class RecObjEsp(Resource):
+    def get(self, numero):
+        pass
+
+    def post(self):
+        pass
+
+
+api.add_resource(RecObj, '/test')
+api.add_resource(RecObjEsp, '/test/<int:id>')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
